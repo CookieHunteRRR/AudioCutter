3a45ab7aacff9f228996b0a8c702c38068e35110
CookieHunteRRR <cookievip2014@gmail.com>
2023-05-19 12:33:12 +0300

    Поддержка WAV-файлов и некорректный ввод пути не ломает скрипт

--- AudioCutter/Program.cs
+++ AudioCutter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using NAudio.Wave;
+using System.IO;
+using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace AudioCutter
@@ -21,20 +23,56 @@ namespace AudioCutter
 
             while (true)
             {
-                Console.WriteLine("Введите путь к MP3 файлу:");
+                Console.WriteLine("Введите путь к MP3/WAV файлу:");
                 string input = Console.ReadLine();
-                TrimMP3(input);
+                string ext = Path.GetExtension(input);
+
+                if (ext == ".mp3")
+                {
+                    TrimMP3(input);
+                }
+                else if (ext == ".wav")
+                {
+                    TrimWAV(input);
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный формат или путь файла.");
+                }
+            }
+        }
+
+        public static void TrimWAV(string inputPath)
+        {
+            string inputFileName = Path.GetFileName(inputPath);
+            string outputFolderName = $"output_{inputFileName}";
+            var outputFolderInfo = Directory.CreateDirectory(outputFolderName);
+
+            using (var reader = new AudioFileReader(inputPath))
+            {
+                int cutCount = 0;
+                bool isFileOver = false;
+                var cutAsTimeSpan = TimeSpan.FromSeconds(cutLengthInSeconds);
+
+                while (!isFileOver)
+                {
+                    CreateNewWAVCut(outputFolderInfo.FullName, inputFileName, cutCount, reader.Take(cutAsTimeSpan));
+
+                    cutCount++;
+                    // Если следующий отрезок выйдет за длительность файла
+                    if (reader.CurrentTime + cutAsTimeSpan > reader.TotalTime)
+                    {
+                        cutAsTimeSpan = reader.TotalTime - reader.CurrentTime;
+                        isFileOver = true;
+                    }
+                }
+                CreateNewWAVCut(outputFolderName, inputFileName, cutCount, reader.Take(cutAsTimeSpan));
+                Console.WriteLine($"Файл порезан");
             }
         }
 
         public static void TrimMP3(string inputPath)
         {
-            if (!File.Exists(inputPath))
-            {
-                Console.WriteLine("Путь не существует\n");
-                return;
-            }
-
             string inputFileName = Path.GetFileName(inputPath);
             string outputFolderName = $"output_{inputFileName}";
             var outputFolderInfo = Directory.CreateDirectory(outputFolderName);
@@ -47,7 +85,7 @@ namespace AudioCutter
                 int savedFrames = 0;
                 int cutCount = 0;
 
-                FileStream nextCut = CreateNewCut(outputFolderInfo.FullName, inputFileName, cutCount);
+                FileStream nextCut = CreateNewMP3Cut(outputFolderInfo.FullName, inputFileName, cutCount);
                 while ((frame = reader.ReadNextFrame()) != null)
                 {
                     nextCut.Write(frame.RawData, 0, frame.RawData.Length);
@@ -59,8 +97,7 @@ namespace AudioCutter
                         savedFrames = 0;
 
                         nextCut.Close();
-                        nextCut = CreateNewCut(outputFolderInfo.FullName, inputFileName, cutCount);
-                        Console.WriteLine($"Сохранено {cutCount}");
+                        nextCut = CreateNewMP3Cut(outputFolderInfo.FullName, inputFileName, cutCount);
                     }
                 }
 
@@ -69,7 +106,13 @@ namespace AudioCutter
             }
         }
 
-        private static FileStream CreateNewCut(string folderPath, string inputFileName, int cutCount)
+        private static void CreateNewWAVCut(string folderPath, string inputFileName, int cutCount, ISampleProvider cutAsSampleProvider)
+        {
+            string outputPath = folderPath + $"\\{inputFileName}_{cutCount}.wav";
+            WaveFileWriter.CreateWaveFile16(outputPath, cutAsSampleProvider);
+        }
+
+        private static FileStream CreateNewMP3Cut(string folderPath, string inputFileName, int cutCount)
         {
             return new FileStream(folderPath + $"\\{inputFileName}_{cutCount}.mp3", FileMode.Create, FileAccess.Write);
         }
